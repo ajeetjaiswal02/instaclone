f67b03c2505e3865339a0eb6445cdb774fc995f5
ajeetjaiswal02 <ajeetjaiswal103@gmail.com>
2020-10-04 21:44:39 +0530

    "update"

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -1,7 +1,10 @@
 import { Button } from '@material-ui/core';
 import React, { useState } from 'react';
+import { storage, db } from "./firebase";
 
-function ImageUpload() {
+
+
+function ImageUpload({username}) {
     const [image, setImage] = useState(null);
     const [progress, setProgress] = useState(0);
     const [caption, setCaption] = useState('');
@@ -14,6 +17,35 @@ function ImageUpload() {
     };
 
     const handleUpload = () => {
+        const uploadTask = storage.ref(`images/${image.name}`).put(image);
+ 
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const progress = Math.round(
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                );
+                setProgress(progress);
+            },
+            (error) => {
+                console.log(error);
+                alert(error.message);
+            },
+            () => {
+                storage
+                  .ref("images")
+                  .child(image.name)
+                  .getDownloadURL()
+                  .then(url => {
+                      db.collection("posts").add({
+                          timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                          caption: caption,
+                          imageUrl: url,
+                          username: username                          
+                      })
+                  })
+            }
+        )
 
     }
 
